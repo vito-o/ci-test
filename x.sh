8ff66b21068c39c53a8f2551f0955fd05f758c70
wt-demos <taiwangwan@163.com>
2025-06-12 16:57:37 +0800

    update

--- x.sh
+++ x.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# 强制删除所有基于该镜像的容器
+# 删除旧容器
 docker rm -f $(docker ps -a -q --filter ancestor=react-demo:latest) || true
+
 # 删除旧镜像
-docker rmi react-demo:latest || true
+docker rmi -f react-demo:latest || true
 
-# 构建镜像
-echo "🛠️ 构建镜像"
-docker build -t react-demo /home/vito/.jenkins/workspace/web-task
+echo "🛠️ 开始强制重建镜像..."
+docker build --no-cache -t react-demo /home/vito/.jenkins/workspace/web-task
 
-# 运行容器
-echo "🚀 启动容器"
-docker run -d -p 8083:8083 --name react-demo  react-demo
+echo "🚀 启动新容器..."
+docker run -d -p 8083:8083 --name react-demo react-demo
 
-echo "📦 当前运行容器列表"
+echo "📦 当前运行容器："
 docker ps
-
